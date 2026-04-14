74b044229d413cb10ef7b6838be847b147f402e0
Maryam Altammam <mrymst03@gmail.com>
2026-04-14 09:56:06 +0300

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import { useNavigate } from "react-router-dom"
 const Home = () => {
-  return <h1>Home Page</h1>
+  const navigate = useNavigate()
+  return (
+    <div className="home">
+      <h1 className="homeTitle">Welcome to FunLand 🎡</h1>
+
+      <p className="homeText">
+        Book your tickets easily and enjoy a fun day full of exciting rides!
+      </p>
+
+      <button className="homeBtn" onClick={() => navigate('/book')} >Book Now</button>
+    </div>
+  )
 }
 
 export default Home
