f6930c5f0105b851f6e9179e992621e949b3779f
Maryam Altammam <mrymst03@gmail.com>
2026-04-14 09:50:55 +0300

    About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,22 @@
 const About = () => {
-  return <h1>About Page</h1>
+  return (
+    <div className="aboutContainer">
+
+      <section className="about">
+        <h1 className="aboutTitle">About FunLand 🎡</h1>
+
+        <p className="aboutText">
+          FunLand is your perfect destination for fun, adventure, and unforgettable memories.
+          We offer exciting rides, family-friendly attractions, and a simple booking experience.
+        </p>
+
+        <p className="aboutText">
+          Whether you're visiting with friends or family, FunLand guarantees a day full of joy and excitement.
+        </p>
+      </section>
+
+    </div>
+  )
 }
 
 export default About
